bede7c8d384168e73b47f2846b757f5ff10a2024
Boone Reynolds <boone.reynolds@gmail.com>
2017-03-23 14:34:50 -0700

    creates files for api routes and controller

---
+++ config/routes/api.js
@@ -0,0 +1,7 @@
+var express = require('express'),
+    router = new Express.router()
+    //more stuff from controller here
+
+
+
+router.route('/api')
---
+++ controllers/api.js
@@ -0,0 +1,11 @@
+var Proposal = require('../models/proposal')
+
+
+function index(req, res) {
+
+}
+
+
+function showAPI (req, res) {
+
+}
