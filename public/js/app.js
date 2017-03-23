2259c260b2cf2b2c5534563308537fc7dd39981f
Elizabeth Donohue <elizabeth.r.donohue@gmail.com>
2017-03-22 18:52:02 -0700

    fixing branch switch

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,30 @@
 console.log("header partial is connected")
 
+
 $(document).ready(function() {
   $yaybutton = $('#yay')
   $naybutton = $('#nay')
 
-  $yaybutton.click(function() {
-    $.post('/proposals')
-  })
 
-})
+  // $yaybutton.click(function() {
+  //   $.post('/proposals')
+  // })
 
-
+$('#yaycounter').data('count', 0);
 ("#yay").click(function() {
-  var yaycounter = 0;
-    $('#counter').html(function(i, val) {
-        $.ajax({
-            url: '/proposals',
-            type: 'POST',
-            data: {increment: true},
-            success: function() { alert('Request has returned') }
-        });
-        return +val+1;
-        console.log(+val+1)
+    $('#yaycounter').html(function() {
+      var $this = $(this),
+        count = $this.data('count') + 1;
+
+        $this.data('count', count);
+        console.log(count);
     });
-}
+});
+});
+
+        // $.ajax({
+        //     url: '/proposals',
+        //     type: 'POST',
+        //     data: {increment: true},
+        //     success: function() { alert('Request has returned') }
+// <div id="counter"></div>
